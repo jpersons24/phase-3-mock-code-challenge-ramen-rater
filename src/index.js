6ec0d7240292bc07e3b91471664868c89d893c05
Jakob Persons <jakobep24@gmail.com>
2021-01-20 09:35:27 -0500

    first deliverable

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-// write your code here
+
+// VARIABLES
+
+const imgContainer = document.querySelector('#ramen-menu')
+
+
+
+// NETWORK REQUESTS
+
+// fetch('http://example.com/movies.json')
+//   .then(response => response.json())
+//   .then(data => console.log(data));
+
+getAllRamens()
+
+function getAllRamens() {
+   fetch("http://localhost:3000/ramens")
+   .then(response => response.json())
+   .then(allRamenObjects => {
+      // console.log(allRamenObjects)
+      allRamenObjects.forEach(ramenObj => {
+         renderRamenObj(ramenObj)
+      })
+   })
+}
+
+
+
+// RENDER FUNCTIONS
+
+
+function renderRamenObj(ramenObj) {
+   const imgTag = document.createElement('img')
+   imgTag.setAttribute('src', ramenObj.image)
+   imgTag.setAttribute('alt', 'Image of menu item')
+
+   imgContainer.append(imgTag)
+}
